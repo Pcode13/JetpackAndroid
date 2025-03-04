c1c40e54fa40b2d3e4699c876b07b236f7714977
Pragati Veer <92431808+Pcode13@users.noreply.github.com>
2025-03-04 17:47:30 +0530

    Upload the image and Style on images

--- app/src/main/java/com/example/myapplication2/MainActivity.kt
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.myapplication2
 
+import android.media.Image
 import android.os.Bundle
+import android.provider.MediaStore.Images
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -19,59 +23,77 @@ import com.example.myapplication2.ui.theme.MyApplication2Theme
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.magnifier
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 
-data class Restaurant(val name: String, val tagLine: String)
+data class Restaurant(
+    val name: String,
+    val tagLine: String,
+    val imageUrl: Int,
+)
 
 val restaurants = listOf(
     Restaurant(
         name = "Pizza Pizza",
-        tagLine = "Italian Pizza"
+        tagLine = "Italian Pizza",
+        imageUrl = R.drawable.res_img1
     ),
     Restaurant(
         name = "Pizza Pizza",
-        tagLine = "Italian Pizza"
+        tagLine = "Italian Pizza",
+        imageUrl = R.drawable.res_img2
+    ),
+    Restaurant(
+        name = "Pizza Pizza",
+        tagLine = "Italian Pizza",
+        imageUrl = R.drawable.res_img3
     ),
 
-//    Restaurant(
-//        name = "Pizza Pizza",
-//        tagLine = "Italian Pizza"
-//    ),
-//
-//    Restaurant(
-//        name = "Pizza Pizza",
-//        tagLine = "Italian Pizza"
-//    ),
-//    Restaurant(
-//        name = "Pizza Pizza",
-//        tagLine = "Italian Pizza"
-//    ),
+    Restaurant(
+        name = "Pizza Pizza",
+        tagLine = "Italian Pizza",
+        imageUrl = R.drawable.res_img4
+    ),
+
+    Restaurant(
+        name = "Pizza Pizza",
+        tagLine = "Italian Pizza",
+        imageUrl = R.drawable.res_img5
 
     )
 
-val HorizontalRestaurantList = listOf(
-    Restaurant(
-        name = "Piz",
-        tagLine = "Ital"
-    ),
-    Restaurant(
-        name = "P",
-        tagLine = "Ita"
-    ),
-    Restaurant(
-        name = "Piz",
-        tagLine = "Ital"
-    ),
-    Restaurant(
-        name = "Z",
-        tagLine = "Itali"
-    ),
-    Restaurant(
-        name = "Pizz",
-        tagLine = "Italin"
-    ),
 )
+
+//val HorizontalRestaurantList = listOf(
+//    Restaurant(
+//        name = "Piz",
+//        tagLine = "Ital"
+//    ),
+//    Restaurant(
+//        name = "P",
+//        tagLine = "Ita"
+//    ),
+//    Restaurant(
+//        name = "Piz",
+//        tagLine = "Ital"
+//    ),
+//    Restaurant(
+//        name = "Z",
+//        tagLine = "Itali"
+//    ),
+//    Restaurant(
+//        name = "Pizz",
+//        tagLine = "Italin"
+//    ),
+//)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -80,11 +102,12 @@ class MainActivity : ComponentActivity() {
         setContent {
 
             MyApplication2Theme {
-                Surface(modifier = Modifier.fillMaxSize(),
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ){
-//                    RestaurantList(restaurants)
-                    HorizontalRestaurantList(HorizontalRestaurantList)
+                ) {
+                    RestaurantList(restaurants)
+//                    HorizontalRestaurantList(HorizontalRestaurantList)
                 }
             }
         }
@@ -92,52 +115,71 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Composable
-fun HorizontalRestaurantList(horizontalRestaurantList: List<Restaurant>) {
-Row(verticalAlignment = Alignment.CenterVertically) {
-    for (restaurant in horizontalRestaurantList) {
-        RestaurantCard(restaurant=restaurant)
-    }
-}
-}
+//@Composable
+//fun HorizontalRestaurantList(horizontalRestaurantList: List<Restaurant>) {
+//    Row(verticalAlignment = Alignment.CenterVertically) {
+//        for (restaurant in horizontalRestaurantList) {
+//            RestaurantCard(restaurant = restaurant)
+//        }
+//    }
+//}
 
 
 @Composable
 fun RestaurantList(restaurants: List<Restaurant>) {
-Column(verticalArrangement = Arrangement.Bottom) {
-    for (restaurant in restaurants) {
-    RestaurantCard(restaurant=restaurant)
+    Column(verticalArrangement = Arrangement.SpaceEvenly) {
+        for (restaurant in restaurants) {
+            RestaurantCard(restaurant = restaurant)
+        }
     }
-}
 }
 
 @Composable
 fun RestaurantCard(restaurant: Restaurant) {
     Surface(
         shape = MaterialTheme.shapes.medium,
-        shadowElevation = 2.dp,
-        color = Color.LightGray,
-//        modifier = Modifier.fillMaxWidth() it is for Vertically aligned
-        modifier = Modifier.height(80.dp)
+        shadowElevation = 4.dp,
+        color = Color.White,
+        modifier = Modifier.fillMaxWidth()
+//        modifier = Modifier.height(80.dp)
 
     ) {
-        Column {
-            Text(
-                restaurant.name,
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.padding(
-                    start = 32.dp, top = 32.dp,
-                    end = 32.dp, bottom = 2.dp
+
+
+        Row(Modifier.padding(8.dp)) {
+            Image(
+                painter = painterResource(id = restaurant.imageUrl),
+                contentDescription = "Restaurant Image",
+                modifier = Modifier.size(100.dp)
+                    .clip(CircleShape)
+                    .border(width = 2.dp,
+                        color = Color.Green,
+                        shape = CircleShape),
+                contentScale = androidx.compose.ui.layout.ContentScale.Crop
+
                 )
-            )
-            Text(
-                restaurant.tagLine,
-                style = MaterialTheme.typography.headlineSmall,
-                modifier = Modifier.padding(
-                    start = 32.dp, top = 2.dp,
-                    end = 32.dp, bottom = 32.dp
+            Spacer( modifier = Modifier.width(8.dp))
+
+            Column(verticalArrangement = Arrangement.Center) {
+                Text(
+                    restaurant.name,
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(
+                        start = 16.dp,
+                        end = 16.dp,
+                    )
                 )
-            )
+
+                Spacer(Modifier.height(4.dp))
+                Text(
+                    restaurant.tagLine,
+                    style = MaterialTheme.typography.labelSmall,
+                    modifier = Modifier.padding(
+                        start = 16.dp,
+                        end = 16.dp,
+                    )
+                )
+            }
         }
     }
 
@@ -145,20 +187,22 @@ fun RestaurantCard(restaurant: Restaurant) {
 }
 
 
-
 @Preview(showBackground = true, name = "cardPerview", backgroundColor = 0xFF4CAF50)
 @Composable
 fun RestaurantCardPreview() {
     MyApplication2Theme {
-       RestaurantCard(
-           Restaurant(
-               name = "Pizza Pizza",
-               tagLine = "Italian Pizza Demo"
-           )
-       )
+        RestaurantCard(
+            Restaurant(
+                name = "Pizza Pizza",
+                tagLine = "Italian Pizza Demo",
+                imageUrl = R.drawable.res_img1
+            )
+        )
     }
 }
-@Preview(showBackground = true, name = "listPerview", backgroundColor = 0xFFFF5722,
+
+@Preview(
+    showBackground = true, name = "listPerview", backgroundColor = 0xFFFF5722,
     showSystemUi = true
 )
 @Composable
